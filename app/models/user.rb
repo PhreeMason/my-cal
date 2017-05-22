0dff0d7859d98a4226c37adba5f8ab0c34686e93
Omar Mason <mr.omarmason@gmail.com>
2017-05-22 21:10:16 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :calenday       
   has_many :mytasks
-  has_one :calendar
   has_many :tasks, through: :mytasks
   # before_save :give_me_calendar
-  
+
   # def give_me_calendar
   #   if !self.persisted?
   #     self.calendar.create()
