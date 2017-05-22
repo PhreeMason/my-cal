fec5fe30a481485ff10a26aa2d6a56254017a32d
Omar Mason <mr.omarmason@gmail.com>
2017-05-22 22:27:25 +0000

    fixed the facebook login features

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   has_one :calendar
   has_many :mytasks
@@ -13,6 +13,7 @@ class User < ApplicationRecord
     self.build_calendar
   end
   def self.from_omniauth(auth)
+    binding.pry
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.provider = auth.provider
        user.uid = auth.uid
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#home"
   #rails s -p $PORT -b $IP
