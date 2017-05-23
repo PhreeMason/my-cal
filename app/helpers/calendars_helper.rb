7d055c1b0469f95a79b593bab74850e45a5a0ab5
Omar Mason <mr.omarmason@gmail.com>
2017-05-23 08:03:26 +0000

    autopopulated last week of month on show page

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -17,7 +17,7 @@ module CalendarsHelper
     now = DateTime.now - 1.month
     month = my_cal.months.find_by(order: now.month, year: now.strftime("%Y"))
     if month
-      month.first_week_days
+      month.last_week_days
     else
       my_cal.add_month(now).last_week_days
     end
@@ -27,9 +27,9 @@ module CalendarsHelper
     now = DateTime.now + 1.month
     month = my_cal.months.find_by(order: now.month, year: now.strftime("%Y"))
     if month
-      month.last_week_days
+      month.first_week_days
     else
-      my_cal.add_month(now).last_week_days
+      my_cal.add_month(now).first_week_days
     end  
   end
   
