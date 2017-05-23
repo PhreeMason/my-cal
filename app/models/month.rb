12c0981f5e9f7db9e3330985253042837485edcf
Omar Mason <mr.omarmason@gmail.com>
2017-05-23 07:19:57 +0000

    created method to get all days within the middle of a month.

--- app/models/month.rb
+++ app/models/month.rb
@@ -6,11 +6,23 @@ class Month < ApplicationRecord
     week_days = []
     day_of_week  = DateTime.new(year, order).wday
     n = 7 - day_of_week
-    (1..m).each { |e| week_days << e }
+    (1..n).each { |e| week_days << e }
     week_days
   end
 
-  def last_few_days
+  def middle_week_days
+    week_days = []
+    start = first_week_days.last + 1
+    finish = last_week_days.first - 1
+    all_days = (start..finish).map { |e| e }
+    until all_days.empty?
+      week_days << all_days.slice!(0..6)
+    end
+    week_days
+  end
+
+
+  def last_week_days
     week_days = []
     day_of_week = DateTime.new(year, order).end_of_month.wday
     n = days - day_of_week
